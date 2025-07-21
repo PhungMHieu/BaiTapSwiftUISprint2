b4c774eeaa88bbb86f7389306b92547df9e6a1ff
PhungMHieu <HieuPM.B21cn368@stu.ptit.edu.vn>
2025-07-21 08:33:56 +0700

    Initial Commit

---
+++ BaiTapSwiftUISprint2/BaiTapSwiftUISprint2App.swift
@@ -0,0 +1,17 @@
+//
+//  BaiTapSwiftUISprint2App.swift
+//  BaiTapSwiftUISprint2
+//
+//  Created by iKame Elite Fresher 2025 on 21/7/25.
+//
+
+import SwiftUI
+
+@main
+struct BaiTapSwiftUISprint2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BaiTapSwiftUISprint2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BaiTapSwiftUISprint2
+//
+//  Created by iKame Elite Fresher 2025 on 21/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
